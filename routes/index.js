7256382aa970e73348d811fbc8159cb57f379a46
Nuria de los Reyes <nuria.rhipola@alumnos.upm.es>
2016-04-26 14:38:58 +0200

    creditos

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
-
+router.get('/author', function(req, res, next) {
+  res.render('author', { author:'Nuria de los Reyes' , url:'https://github.com/nuriadelosreyes/QuizRehecho.git' });
+});
 module.exports = router;
